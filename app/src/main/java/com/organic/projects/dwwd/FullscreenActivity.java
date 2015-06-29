82425b0fed91742612b7c2c1ed4c3ffcf6cc088b
Jordan Gray <jgray@organic.com>
2015-06-29 02:23:21 -0700

    i love you hash map

--- app/src/main/java/com/organic/projects/dwwd/FullscreenActivity.java
+++ app/src/main/java/com/organic/projects/dwwd/FullscreenActivity.java
@@ -1,31 +1,28 @@
 package com.organic.projects.dwwd;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.gimbal.android.BeaconEventListener;
 import com.gimbal.android.BeaconManager;
 import com.gimbal.android.BeaconSighting;
-import com.gimbal.android.Communication;
-import com.gimbal.android.CommunicationListener;
-import com.gimbal.android.CommunicationManager;
 import com.gimbal.android.Gimbal;
-import com.gimbal.android.PlaceEventListener;
-import com.gimbal.android.PlaceManager;
-import com.gimbal.android.Push;
-import com.gimbal.android.Visit;
 import com.organic.projects.dwwd.util.SystemUiHider;
 
-import java.sql.Date;
-import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
 
 
 /**
@@ -61,187 +58,139 @@ public class FullscreenActivity extends Activity {
     /**
      * The instance of the {@link SystemUiHider} for this activity.
      */
-    private SystemUiHider mSystemUiHider;
 
-    private PlaceEventListener placeEventListener;
-    private CommunicationListener communicationListener;
     private BeaconEventListener beaconSightingListener;
     private BeaconManager beaconManager;
 
-    private final int interval = 100;
+    private Integer previousRSSI = 1111;
+    private String previousName = "";
+
+    private static String UNDER_WATER = "UNDER WATER";
+    private static String OUTER_SPACE = "OUTER SPACE";
+    private Map<String, Drawable> locations;
+    private String currentLocation = UNDER_WATER;
+
+    private final int interval = 10;
     private Handler handler = new Handler();
+
+    private LinearLayout mProgress;
+    private Button mLocationToggle;
+    private ImageView mBackground;
+
+    private static Integer requiredSignalStrength = 55;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        locations = new HashMap<String, Drawable>();
+        locations.put(UNDER_WATER, getResources().getDrawable(R.drawable.ocean_50p));
+        locations.put(OUTER_SPACE, getResources().getDrawable(R.drawable.outer_space));
+
         Gimbal.setApiKey(this.getApplication(), "dde26c72-ea4c-411e-85f8-3f1ddb9c45dc");
 
-        placeEventListener = new PlaceEventListener() {
+        beaconSightingListener = new BeaconEventListener() {
             @Override
-            public void onVisitStart(Visit visit) {
-                // This will be invoked when a place is entered. Example below shows a simple log upon enter
-                Log.i("Info:", "Enter: " + visit.getPlace().getName() + ", at: " + new Date(visit.getArrivalTimeInMillis()));
-            }
-
-            @Override
-            public void onVisitEnd(Visit visit) {
-                // This will be invoked when a place is exited. Example below shows a simple log upon exit
-                Log.i("Info:", "Exit: " + visit.getPlace().getName() + ", at: " + new Date(visit.getDepartureTimeInMillis()));
+            public void onBeaconSighting(BeaconSighting sighting) {
+                Log.i("INFO", sighting.toString());
+                previousName = sighting.getBeacon().getName();
+                previousRSSI = sighting.getRSSI() * -1;
             }
         };
-        PlaceManager.getInstance().addListener(placeEventListener);
+        beaconManager = new BeaconManager();
+        beaconManager.addListener(beaconSightingListener);
 
-        communicationListener = new CommunicationListener() {
-            @Override
-            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Visit visit) {
-                for (Communication comm : communications) {
-                    Log.i("INFO", "Place Communication: " + visit.getPlace().getName() + ", message: " + comm.getTitle());
-                }
-                //allow Gimbal to show the notification for all communications
-                return communications;
-            }
-
-            @Override
-            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Push push) {
-                for (Communication comm : communications) {
-                    Log.i("INFO", "Received a Push Communication with message: " + comm.getTitle());
-                }
-                //allow Gimbal to show the notification for all communications
-                return communications;
-            }
-
-            @Override
-            public void onNotificationClicked(List<Communication> communications) {
-                Log.i("INFO", "Notification was clicked on");
-            }
-        };
-
-        CommunicationManager.getInstance().addListener(communicationListener);
-
-        GimbalRSSI gimbalRSSI = new GimbalRSSI();
-        gimbalRSSI.manageRSSI(this);
+        beaconManager.startListening();
 
         setContentView(R.layout.activity_fullscreen);
 
         handler.postDelayed(changeProgressBar, interval);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
+        mLocationToggle = (Button) findViewById(R.id.location_toggle);
+        mBackground = (ImageView) findViewById(R.id.background);
 
-         // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
+        mLocationToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
+                Iterator it = locations.entrySet().iterator();
+                Boolean foundCurrent = false;
+                Boolean sawFirst = false;
+                String firstKey = "";
+                Boolean weDone = false;
+                while (it.hasNext() && !weDone) {
+                    Map.Entry pair = (Map.Entry) it.next();
+                    if (!sawFirst) {
+                        sawFirst = true;
+                        firstKey = pair.getKey().toString();
+                    }
+                    if (foundCurrent) {
+                        Drawable background = locations.get(pair.getKey());
+                        currentLocation = pair.getKey().toString();
+                        mBackground.setImageDrawable(background);
+                        weDone = true;
+                    }
+                    if (pair.getKey() == currentLocation) {
+                        foundCurrent = true;
+                    }
+                }
+                if (!weDone) {
+                    Drawable background = locations.get(firstKey);
+                    currentLocation = firstKey.toString();
+                    mBackground.setImageDrawable(background);
                 }
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     /**
      * Change progress bar
+     *
      * @param RSSI
      */
-    private Runnable changeProgressBar = new Runnable(){
+    private Runnable changeProgressBar = new Runnable() {
         @Override
         public void run() {
-            System.out.println("DRAWER: " + GimbalRSSI.previousRSSI);
+            mLocationToggle.setVisibility(View.VISIBLE);
+            TextView fullscreenContent = (TextView) findViewById(R.id.fullscreen_content);
+            fullscreenContent.setText(previousName + " outside beam range");
+
+            TextView fullscreenContentBig = (TextView) findViewById(R.id.fullscreen_content_big);
+            fullscreenContentBig.setText(previousRSSI + "\nAWAY");
+
+            Integer width = 0;
+            mProgress = (LinearLayout) findViewById(R.id.progress);
+
+            System.out.println("DRAWER: " + previousRSSI);
             handler.postDelayed(this, interval);
-        }
-    };
 
+            if (previousRSSI < requiredSignalStrength) {
+                mLocationToggle.setVisibility(View.GONE);
+                width = ViewGroup.LayoutParams.MATCH_PARENT;
+                fullscreenContent.setText("ILLICIT TRANSACTION COMPLETE. \n\nNEXT STOP:");
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+                HashMap<String, Drawable> locationsCopy = new HashMap<String, Drawable>();
+                locationsCopy.putAll(locations);
+                locationsCopy.remove(currentLocation);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
+                System.out.println("HEY Length of locations " + locations.size());
+                System.out.println("HEY length of locationsCopy " + locationsCopy.size());
 
+                Random generator = new Random();
+                Object[] keys = locationsCopy.keySet().toArray();
+                Object randomKey = keys[generator.nextInt(keys.length)];
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                fullscreenContentBig.setText(randomKey.toString());
             }
-            return false;
+
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width, ViewGroup.LayoutParams.MATCH_PARENT);
+            mProgress.setLayoutParams(params);
+
+            // error state
+            if (previousRSSI == 1111) {
+                fullscreenContent.setText("Drone not detected. Try toggling BLE.");
+                fullscreenContentBig.setText("");
+            }
         }
     };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
 }
+
